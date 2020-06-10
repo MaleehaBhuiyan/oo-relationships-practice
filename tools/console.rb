def031c3ec5d75756c4a60d12160437268ccf790
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-06-09 23:24:51 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,26 @@ def reload
   load 'config/environment.rb'
 end
 
+#############################################
+### LISTINGS ###
+one = Listing.new("NYC")
+two = Listing.new("Paris")
+three = Listing.new("Cape Cod")
+#############################################
+### GUESTS ###
+liz = Guest.new("Liz")
+sue = Guest.new("Sue")
+ann = Guest.new("Ann")
+mario = Guest.new("Mario")
+tim = Guest.new("Tim")
+#############################################
+### TRIPS ### 
+t_one = Trip.new(one,liz)
+t_two = Trip.new(two,sue)
+t_three = Trip.new(three,ann)
+t_four = Trip.new(one,mario)
+t_five = Trip.new(two,tim)
+t_six = Trip.new(three,tim)
+t_seven = Trip.new(two,mario) 
 binding.pry
 0
